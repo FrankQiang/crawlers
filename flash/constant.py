004f3fdb4b9ba81d3892557a1a6d2bd43009305a
yanklu <yank_lu@126.com>
2016-04-26 15:51:45 +0800

    delete constant settings

--- flash/constant.py
+++
@@ -1,4 +0,0 @@
-LOCAL_HOST = 'http://127.0.0.1:8080'
-SINGLE_URL = LOCAL_HOST+'/prode/single/?url='
-AMAZON_HOST = 'http://www.amazon.com'
-AMAZON_URL = AMAZON_HOST+'/s?ie=UTF8&page={page}&rh=i:aps,k:{keywords}'
