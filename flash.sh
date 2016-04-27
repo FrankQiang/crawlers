4d9d7b5dd5a82c4ae9dfd4ba303b336e53292174
yanklu <yank_lu@126.com>
2016-04-27 16:04:52 +0800

    add stop sh

--- flash.sh
+++ flash.sh
@@ -2,11 +2,17 @@ start(){
     supervisorctl -c /etc/supervisord.conf restart flash
 }
 
+stop(){
+    supervisorctl -c /etc/supervisord.conf stop flash
+}
+
 case "$1" in
   start) start ;;
+  stop) stop ;;
   *)  
     echo "Usage:"
     echo "./flash.sh start"
+    echo "./flash.sh stop"
     exit 1
     ;;  
 esac
